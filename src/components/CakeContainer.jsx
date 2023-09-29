0e67f82fda9a6927e7e6dccf1813aae57adb3131
Irakoze Aime Aubin <irakozeaimeaubin@gmail.com>
2023-09-29 16:51:30 +0200

    commit #1

---
+++ src/components/CakeContainer.jsx
@@ -0,0 +1,25 @@
+import { buyCake } from "../redux";
+import { connect } from "react-redux";
+
+function CakeContainer (props) {
+    return (
+        <div className="mx-auto flex flex-col gap-2">
+            <h2 className="font-medium text-lg tracking-wide">Number of cakes - {props.numOfCakes}</h2>
+            <button className="text-white uppercase tracking-widest font-semibold bg-amber-700 px-5 py-2.5 rounded hover:bg-amber-800 focus:ring-4 focus:ring-amber-300" onClick={props.buyCake}>Buy Cake</button>
+        </div>
+    );
+}
+
+const mapStateToProps = state => {
+    return {
+        numOfCakes: state.numOfCakes
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        buyCake: () => dispatch( buyCake() )
+    };
+};
+
+export default connect( mapStateToProps, mapDispatchToProps )( CakeContainer );
---
+++ src/components/HooksCakeContainer.jsx
@@ -0,0 +1,10 @@
+function HooksCakeContainer() {
+  return (
+      <div className="mx-auto flex flex-col gap-2">
+          <h2 className="font-medium text-lg tracking-wide">Number of cakes - {}</h2>
+          <button className="text-white uppercase tracking-widest font-semibold bg-amber-700 px-5 py-2.5 rounded hover:bg-amber-800 focus:ring-4 focus:ring-amber-300">Buy Cake</button>
+      </div>
+  )
+}
+
+export default HooksCakeContainer
